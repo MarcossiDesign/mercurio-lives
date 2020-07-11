0f362892eee5d383e40e4738de49c2260949632f
Kinark <igormarcossi@gmail.com>
2020-07-11 13:08:29 -0300

    Added error and constant

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,6 @@ export default {
   LOCALES_FOLDER_NAME: 'locales',
   MERCURIO_TAG_NAME: 'Mercurio',
   BABEL_PLUGIN_NAME: 'babel-mercurio-helmet',
-  BABEL_CONF_FILE_NAMES: ['.babelrc', 'babel.config']
+  BABEL_CONF_FILE_NAMES: ['.babelrc', 'babel.config'],
+  TRANSLATION_FILE_ALIAS: 'mercurioTranslationsFile'
 }
--- src/webpack-mercurio-boots.js
+++ src/webpack-mercurio-boots.js
@@ -8,8 +8,10 @@ import getBabelConfigFile from './getBabelConfigFile'
 function getHelmetSettings() {
   const babelConfigFile = getBabelConfigFile()
 
+  if (!babelConfigFile) return false
+
   const helmetConfig = babelConfigFile.plugins.find((plugin) => plugin[0].endsWith(constants.BABEL_PLUGIN_NAME))
-  if (helmetConfig === -1) return
+  if (helmetConfig === -1) return false
 
   const { filesOutput = constants.FILES_OUTPUT, locales = constants.LOCALES } = helmetConfig[1]
   return { filesOutput, locales }
@@ -17,26 +19,34 @@ function getHelmetSettings() {
 
 function getTranslationsLocation() {
   const root = getRoot()
-  const { filesOutput } = getHelmetSettings()
+  const getHelmetSettings = getHelmetSettings()
+  if (!getHelmetSettings) return false
+  const { filesOutput } = getHelmetSettings
   const absoluteFilesOutput = p.join(root, filesOutput, constants.TRANSLATIONS_FILE_NAME)
   return absoluteFilesOutput
 }
 
 class MercurioSail {
   apply(compiler) {
+    // Resolve alias in React components
     compiler.resolverFactory.plugin('resolver normal', (resolver) => {
       resolver.hooks.resolve.tapAsync('MercurioBoots', (params, resolveContext, callback) => {
-        if (params.request.endsWith('mercurioTranslationsFile')) {
-          const modified = { ...params, request: getTranslationsLocation() }
+        if (params.request.endsWith(constants.TRANSLATION_FILE_ALIAS)) {
+          const translationLocation = getTranslationsLocation()
+          if (!translationLocation) throw new Error('Babel plugin not present or misconfigured.')
+          const modified = { ...params, request: translationLocation }
           return resolver.doResolve('resolve', modified, null, resolveContext, callback)
         }
         return callback()
       })
     })
 
+    // Generate the Translations file
     compiler.hooks.afterCompile.tap('after-compile', (compilation) => {
       const root = getRoot()
-      const { filesOutput, locales } = getHelmetSettings()
+      const helmetSettings = getHelmetSettings()
+      if (!helmetSettings) throw new Error('Babel plugin not present or misconfigured.')
+      const { filesOutput, locales } = helmetSettings
       const translations = {}
       const absoluteFilesOutput = p.join(root, filesOutput)
       const localesFiles = p.join(absoluteFilesOutput, constants.LOCALES_FOLDER_NAME)
