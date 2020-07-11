f059cf060fc5e76767aef2d19b52b1f9ecd0e586
Kinark <igormarcossi@gmail.com>
2020-07-11 11:55:04 -0300

    renamed file

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export default {
+  FILES_OUTPUT: './mercurio',
+  LOCALES: ['en'],
+  TRANSLATIONS_FILE_NAME: 'translations.json',
+  LOCALES_FOLDER_NAME: 'locales',
+  MERCURIO_TAG_NAME: 'Mercurio',
+  BABEL_PLUGIN_NAME: 'babel-mercurio-helmet',
+  BABEL_CONF_FILE_NAMES: ['.babelrc', 'babel.config']
+}
--- src/defaults.js
+++
@@ -1,8 +0,0 @@
-export default {
-  filesOutput: './mercurio',
-  locales: ['en'],
-  translationsFileName: 'translations.json',
-  localesFolderName: 'locales',
-  mercurioTagName: 'Mercurio',
-  babelPluginName: 'babel-mercurio-helmet'
-}
